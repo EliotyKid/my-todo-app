8891f7c4524c66eb770f9cd4cd2b706a0ece3dbd
Eliort Kid <eliotfonse99@gmail.com>
2025-01-21 10:09:47 -0300

    form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,33 @@
-import Image from "next/image";
+"use client"
+import TaskForm from "@/components/TaskForm";
+import { Task } from "@/types/task";
+import { getTasks, saveTasks } from "@/utils/localStorageHelper";
+import React, { useEffect, useState } from "react";
 
 export default function Home() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  useEffect(() => {
+    setTasks(getTasks());
+  },[]);
+
+  const addTask = (task: Task): void =>{
+    const newTasks = [...tasks,task];
+    setTasks(newTasks);
+    saveTasks(newTasks);
+  }
+
+  const toggleTaskCompletion = (id: string): void => {
+    const updatedTasks = tasks.map((task) => {
+      return task.id === id ? { ...task, completed: !task.completed} : task
+    });
+    setTasks(updatedTasks);
+    saveTasks(updatedTasks)
+  }
+
   return (
-    <div></div>
+    <div>
+      <TaskForm onAddTask={addTask}/>
+    </div>
   );
 }
---
+++ src/components/TaskForm.tsx
@@ -0,0 +1,46 @@
+"use client"
+import { useState, FormEvent } from "react";
+import { Task } from "@/types/task";
+
+interface TaskFormProps {
+    onAddTask: (task: Task) => void;
+}
+
+export default function TaskForm({onAddTask}: TaskFormProps){
+    const [title, setTitle] = useState<string>("");
+    const [description, setDescription] = useState<string>("");
+
+    const handleSubmit = (e: FormEvent): void => {
+        e.preventDefault();
+        if (!title) return alert("Title is required");
+
+        const newTask: Task = {
+            id: Date.now().toString(),
+            title,
+            description,
+            completed: false,
+            createdAt: new Date().toISOString(),
+        };
+
+        onAddTask(newTask);
+        setTitle("");
+        setDescription("");
+    }
+
+    return(
+        <form onSubmit={handleSubmit} className="flex flex-col items-center">
+            <input
+                type="text"
+                placeholder="Task Title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+            />
+            <textarea 
+                placeholder="Task Description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+            />
+            <button type="submit">Add Task</button>
+        </form>
+    )
+}
